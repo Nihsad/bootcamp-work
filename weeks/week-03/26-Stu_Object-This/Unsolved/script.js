22744165c15535e3e1f48d78091a35bfe99f3f8d
Dashin <nihsadd@gmail.com>
2024-01-08 20:18:58 -0700

    class activities

--- weeks/week-03/26-Stu_Object-This/Unsolved/script.js
+++ weeks/week-03/26-Stu_Object-This/Unsolved/script.js
@@ -1,12 +1,15 @@
 // TODO: What does 'this' refer to?
+// this instance of 'this' refers to the browser window.
 console.log(this);
 
 // TODO: What does 'this' refer to?
+// this instance of 'this' still refers to the object window, but it includes a text string as part of the function.
 function helloThis() { 
    console.log("Inside this function, this is " + this); 
    }
 
 // TODO: What will this log? 
+// this will log the age of the child, plus 10 years due to the included function. so, 20.
 var child = { 
    age: 10,
    ageTenYears: function() {
@@ -15,6 +18,7 @@ var child = {
 };
 
 // TODO: What will this log? 
+// this will log the investment growth! so, 5000 * 1.15, which has a result of 5750.
 var investor = {  
   name: "Cash Saver",
   investment: {
