4e6edfb8c16f90c3d98c2ed5640340edbb53fa05
Dashin <nihsadd@gmail.com>
2024-01-10 21:34:30 -0700

    week 4 adds

---
+++ weeks/04-week/02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,20 @@
+// TODO: What will the following line of code log?
+// logs the entire window object using a direct call.
+console.log("window: ")
+console.log(window);
+
+// TODO: What will the following line of code log?
+// logs the HTML document.
+console.log("window.document: ")
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+// logs the root element; in this case, the 'html' element within an HTML document.
+console.log("document.documentElement: ")
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+// logs the head element within the HTML document.
+console.log("document.head: ")
+console.log(document.head);
+
